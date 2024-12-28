e61d10b2cbc1d619919a1050f1b89124306f2832
MehmetAlgl95 <mehmet.algul.5076@outlook.com>
2024-12-28 15:25:27 +0300

    manage task popup and database connection

---
+++ src/pages/get_tasks.php
@@ -0,0 +1,24 @@
+<?php
+require_once '../config/db.php';
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Tüm görevleri çek
+$query = "
+    SELECT taskId, taskTitle, assignedUserId, taskStatus 
+    FROM tasks
+";
+$result = $conn->query($query);
+
+$tasks = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $tasks[] = $row;
+    }
+}
+
+echo json_encode($tasks);
+$conn->close();
+?>
